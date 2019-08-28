a8a21f3bc4fbfa6a2444597ca4049c2d9baffda0
sranciato <sranciato1@gmail.com>
2019-08-28 17:35:54 +0000

    [LOCAL][UPDATE] Fix pipe_file.c to incorporate extra checks

--- pipe_file.c
+++ pipe_file.c
@@ -6,24 +6,24 @@
  */
 void pipe_file(buf_struct *a)
 {
-	char *path_buf[1000], *semi_buf[1000], *path;
+	char *path_buf[1000], *semi_buf[1000], *path, **test;
 	int i, ext;
 
 	_split_newline(a->rbuf, semi_buf);
 	for (i = 0; semi_buf[i]; i++)
 	{
-		a->hist++;
-		_split(semi_buf[i], a->args);
+		_strcpy(a->history[a->hist++], a->rbuf);
+		check_comment(a->rbuf);
+		if ((check_semiandor(a) == 1))
+                        continue;
+		test = _split(semi_buf[i], a->args);
+		if (test == NULL)
+			continue;
+		if (check_exit(a) == 1)
+                        continue;
 		path = get_path(a->envp);
 		_strcpy(a->path_copy, path);
 		_split(a->path_copy, path_buf);
-		if (a->args[0][0] == '/')
-		{
-			execute(a, a->args[0]);
-			_memset(a->args, 0, sizeof(a->args));
-			_memset(a->dest, 0, 1000);
-			continue;
-		}
 		find_path(path_buf, a->args, a->dest);
 		ext = execute(a, a->dest);
 		_memset(a->args, 0, sizeof(a->args));
